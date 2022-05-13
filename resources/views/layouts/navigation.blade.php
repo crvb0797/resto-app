8ebca7c88812e8112e97ca80891442a27b628ab4
Carlos Villatoro <crvb0797@gmail.com>
2022-05-13 04:29:53 -0600

    Creamos las migraciones y modelos de las siguientes tablas:
    - Category
    - Menu
    - Reservation
    - table
    
    además corrimos las migraciones

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -72,6 +72,12 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+
+            @if (Auth::user()->is_admin)
+                    <x-responsive-nav-link :href="route('admin.index')" :active="request()->routeIs('admin.index')">
+                        {{ __('Admin') }}
+                    </x-responsive-nav-link>
+                    @endif
         </div>
 
         <!-- Responsive Settings Options -->
