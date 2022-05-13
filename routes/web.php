8d25e7b0e6cd71b44be6def4ce9dc8e9cacb0a4e
Carlos Villatoro <crvb0797@gmail.com>
2022-05-13 04:54:40 -0600

    Creamos las vistas y los controladores para cada opción del administrador

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\MenuController;
+use App\Http\Controllers\Admin\TableController;
+use App\Http\Controllers\Admin\ReservationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,6 +17,10 @@ Route::get('/dashboard', function () {
 
 Route::middleware(['auth', 'admin'])->name('admin.')->prefix('admin')->group(function () {
     Route::get('/', [AdminController::class, 'index'])->name('index');
+    Route::resource('/categories', CategoryController::class);
+    Route::resource('/menus', MenuController::class);
+    Route::resource('/tables', TableController::class);
+    Route::resource('/reservation', ReservationController::class);
 });
 
 require __DIR__ . '/auth.php';
